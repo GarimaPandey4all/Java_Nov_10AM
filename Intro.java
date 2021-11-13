72d99d6564f81a3200a1feeba076b56213d75fce
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-11-13 11:11:12 +0530

    Add files via upload

---
+++ Intro.java
@@ -0,0 +1,8 @@
+class Example {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World"); //"" - String: Collection of Chars or more than one char
+        // print the output on the output screen
+
+    }
+}
