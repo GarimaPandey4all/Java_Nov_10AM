ed1df0996c8fa6618b2dc15ca329e2999d709acb
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-12-06 10:14:28 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -3,33 +3,96 @@ package com.brainmentors.java.oops;
 public class Student {
 	
 	//Class Variables/Data Members/Instance Variables
-	int rollno;
-	String name;
-	String phone;
-	String course;
-	double fees;	
+	// Data Hiding
+	private int rollno;
+	private String name;
+	private String phone;
+	private String course;
+	private double fees;
+	private String collegeName;
+	
+	//Default Constructor
+	public Student()
+	{
+		//Called Parameterized Constructor
+		this(101, "Ram Kumar", "32785438654", "MCA", 10000.0);
+		collegeName = "SRCC";
+	}
+	
+	//Parameterized Constructor
+	public Student(int rollno, String name, String phone, String course, double fees)
+	{
+		//constructor chaining
+		//this(); // call default constructor
+		this.rollno = rollno;
+		this.name = name;
+		this.phone = phone;
+		this.course = course;
+		this.fees = fees;
+	}
+	
+	//r, n, p, c, f are local variables
+	
+	//public void takeInput(int r, String n, String p, String c, double f)
+//	public void takeInput(int rollno, String name, String phone, String course, double fees)
+//	{
+//		this.rollno = rollno;
+//		this.name = name;
+//		this.phone = phone;
+//		this.course = course;
+//		this.fees = fees;
+//				
+//		//instance variable = local variable
+////		rollno = r;
+////		name = n;
+////		phone = p;
+////		course = c;
+////		fees = f;
+//	}
+	
+	public void print()
+	{
+		System.out.println("Roll no is: "+this.rollno);
+		System.out.println("Name is: "+name);
+		System.out.println("Phone is: "+phone);
+		System.out.println("Course is: "+course);
+		System.out.println("Fees is: "+fees);
+		System.out.println("College Name is: "+collegeName);
+	}
 	
 	public static void main(String[] args) {
 		
+		//Called Default Constructor
 		Student ram = new Student();
 		
-		System.out.println("Roll no is: "+ram.rollno);
-		System.out.println("Name is: "+ram.name);
-		System.out.println("Phone is: "+ram.phone);
-		System.out.println("Course is: "+ram.course);
-		System.out.println("Fees is: "+ram.fees);
+		//Called Parameterized Constructor
+		//Student ram = new Student(101, "Ram Kumar", "32785438654", "MCA", 10000.0);
 		
-		ram.rollno = 101;
-		ram.name = "Ram Kumar";
-		ram.phone = "2164064686";
-		ram.course = "MCA";
-		ram.fees = 10000.0;
+		//ram.takeInput(101, "Ram Kumar", "32785438654", "MCA", 10000.0);
 		
-		System.out.println("Roll no is: "+ram.rollno);
-		System.out.println("Name is: "+ram.name);
-		System.out.println("Phone is: "+ram.phone);
-		System.out.println("Course is: "+ram.course);
-		System.out.println("Fees is: "+ram.fees);
+		ram.print();
+		
+//		System.out.println("Roll no is: "+ram.rollno);
+//		System.out.println("Name is: "+ram.name);
+//		System.out.println("Phone is: "+ram.phone);
+//		System.out.println("Course is: "+ram.course);
+//		System.out.println("Fees is: "+ram.fees);
+		
+		//ram.print();
+		
+//		ram.rollno = 101;
+//		ram.name = "Ram Kumar";
+//		ram.phone = "2164064686";
+//		ram.course = "MCA";
+//		ram.fees = -10000.0;
+		
+		//ram.print();
+		
+//		System.out.println("Roll no is: "+ram.rollno);
+//		System.out.println("Name is: "+ram.name);
+//		System.out.println("Phone is: "+ram.phone);
+//		System.out.println("Course is: "+ram.course);
+//		System.out.println("Fees is: "+ram.fees);
 		
 		
 	}
